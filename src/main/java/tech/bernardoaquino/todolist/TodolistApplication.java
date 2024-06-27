ed70c844d024c7f7d0af6f1bb71b6a7a01697c6a
Bernardo Aquino <45907775+bernardoaquino@users.noreply.github.com>
2024-06-27 19:10:51 -0300

    fix: Applications comments

--- src/main/java/tech/bernardoaquino/todolist/TodolistApplication.java
+++ src/main/java/tech/bernardoaquino/todolist/TodolistApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TodolistApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TodolistApplication.class, args); // Executa um container tomcat para executar a aplicação
+		SpringApplication.run(TodolistApplication.class, args);
 	}
 
 }
